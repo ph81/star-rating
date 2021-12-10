7d0611be7e618fcf261e7ca3ffa9f3962e894896
Edith M <paperhearts81@gmail.com>
2021-12-09 19:51:16 -0600

    first commit

---
+++ src/components/Star.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//using unicode to display the stars
+const Star = ({ marked, starId }) => {
+   return (
+     <span data-star-id={starId} className="star" role="button">
+       {marked ? '\u2605' : '\u2606'}
+     </span>
+   );
+ };
+
+ export default Star;
---
+++ src/components/StarRating.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import Star from './Star';
+
+const StarRating = ({ value }) => {
+   //initial values
+   const [rating, setRating] = useState(parseInt(value) || 0);
+   const [selection, setSelection] = useState(0);
+ 
+   //updating selected star according to hover
+   const hoverOver = event => {
+     let val = 0;
+     if (event && event.target && event.target.getAttribute('data-star-id')) {
+      val = event.target.getAttribute('data-star-id');
+      console.log(val);
+     }
+     setSelection(val);
+   };
+
+
+   return (
+      <>
+     <div
+       onMouseOut={() => hoverOver(null)}
+       onClick={e => setRating(e.target.getAttribute('data-star-id') || rating)}
+       onMouseOver={hoverOver}
+       className='star-rating'
+     >
+       {Array.from({ length: 5 }, (item, idx) => (
+         <Star
+           starId={idx + 1}
+           key={`star_${idx + 1}`}
+           marked={selection ? selection >= idx + 1 : rating >= idx + 1}
+         />
+       ))}
+     </div>
+     </>
+   );
+ };
+
+export default StarRating;
